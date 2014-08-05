b6308bab2bb46385356fe00882ce941b8133e1ec
tanem <tane.morgan@gmail.com>
2014-08-05 19:59:32 +1200

    Let wall component handle avatar mouseover events

--- browser/avatar.js
+++ browser/avatar.js
@@ -1,34 +1,27 @@
 /** @jsx React.DOM */
 var React = require('react');
-var Tweet = require('./tweet');
 
 module.exports = React.createClass({
   
   propTypes: {
-    profileImage: React.PropTypes.string
+    profileImage: React.PropTypes.string,
+    mouseOverHandler: React.PropTypes.func
   },
 
-  handleMouseOver: function(){
-    var clientRect = this.getDOMNode().getBoundingClientRect();
-    var tweetStyles = {
-      top: clientRect.top + 10,
-      left: clientRect.right + 10
-    };
-    React.renderComponent(
-      <Tweet
-        tweetStyles={tweetStyles} 
-        fullName={this.props.fullName}
-        screenName={this.props.screenName}
-        text={this.props.text}
-        profileImage={this.props.profileImage}
-      />,
-      document.querySelector('.tweet-wrapper')
-    );
+  // Propagate the mouse over event up to the parent component.
+  mouseOverHandler: function(){
+    this.props.mouseOverHandler(this);
   },
 
   render: function() {
     return (
-      <img className="avatar" src={this.props.profileImage} height="48" width="48" onMouseOver={this.handleMouseOver} />
+      <img
+        className="avatar"
+        src={this.props.profileImage}
+        height="48"
+        width="48"
+        onMouseOver={this.mouseOverHandler}
+      />
     );
   }
 
--- browser/wall.js
+++ browser/wall.js
@@ -1,12 +1,34 @@
 /** @jsx React.DOM */
 var React = require('react');
 var Avatar = require('./avatar');
+var Tweet = require('./tweet');
 
 module.exports = React.createClass({
 
   propTypes: {
     tweets: React.PropTypes.array,
     wallStyles: React.PropTypes.object
+  },
+
+  avatarMouseOverHandler: function(avatar){
+    clearTimeout(this.timeoutId);
+    this.timeoutId = setTimeout(function(){
+      var clientRect = avatar.getDOMNode().getBoundingClientRect();
+      var tweetStyles = {
+        top: clientRect.top + 10,
+        left: clientRect.right + 10
+      };
+      React.renderComponent(
+        <Tweet
+          tweetStyles={tweetStyles} 
+          fullName={avatar.props.fullName}
+          screenName={avatar.props.screenName}
+          text={avatar.props.text}
+          profileImage={avatar.props.profileImage}
+        />,
+        document.querySelector('.tweet-wrapper')
+      );
+    }, 500);
   },
 
   render: function() {
@@ -20,9 +42,10 @@ module.exports = React.createClass({
               fullName={tweet.fullName}
               screenName={tweet.screenName}
               text={tweet.text}
+              mouseOverHandler={this.avatarMouseOverHandler}
             />
           );
-        })}
+        }, this)}
       </div>
     );
   }

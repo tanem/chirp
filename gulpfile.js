490ef911f64cf44c6dcff1654ce9c13ceb4a5dbe
tanem <tane.morgan@gmail.com>
2014-08-11 09:07:52 +1200

    Additions to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,23 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
 var recess = require('gulp-recess');
+var jshint = require('gulp-jshint');
+var react = require('gulp-react');
+var gulpif = require('gulp-if');
 
-gulp.task('default', function () {
+gulp.task('css', function(){
   return gulp.src('static/main.css')
     .pipe(watch())
     .pipe(recess())
     .pipe(recess.reporter());
 });
+
+gulp.task('jshint', function(){
+  return gulp.src(['.jshintrc', 'browser/*.js'])
+    .pipe(watch())
+    .pipe(gulpif(/\.js$/, react()))
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('default', ['css', 'jshint']);

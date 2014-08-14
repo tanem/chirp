1f534781ac84abd8a13e21947dd9c020f202d8af
tanem <tane.morgan@gmail.com>
2014-08-14 20:08:44 +1200

    Cull bogus window resize code Add comments

--- browser/main.js
+++ browser/main.js
@@ -4,36 +4,17 @@ var parse = require('transform-parse');
 var clump = require('transform-clump');
 var render = require('./render');
 
-/*
-function debounce(fn, delay) {
-  var id;
-  return function(){
-    clearTimeout(id);
-    id = setTimeout(fn, delay);
-  };
-}
-*/
-
-var numColumns;
-var numRows;
-var wallStyles;
-
-function windowResizeHandler() {
-  numColumns = Math.ceil(window.innerWidth / 48);
-  numRows = Math.ceil(window.innerHeight / 48);
-  wallStyles = {
+// Window dimensions are used to determine how many avatars should be shown.
+var numColumns = Math.ceil(window.innerWidth / 48);
+var numRows = Math.ceil(window.innerHeight / 48);
+var wallStyles = {
     height: numRows * 48,
     width: numColumns * 48
   };
 }
 
-// window.addEventListener('resize', debounce(windowResizeHandler, 1000));
-windowResizeHandler();
-
-// TODO: Unpipe/repipe streams with new info when window is resized?
-
-var socketStream = shoe('/tweets');
-socketStream
+// Bind to the server response stream and render it.
+shoe('/tweets')
   .pipe(split('\r\n'))
   .pipe(parse())
   .pipe(clump(numColumns * numRows))
